529084acfce2be66f86703ec8253c46bee711fad
Privatech <ziga.slatnar.stagar@gmail.com>
2024-02-28 16:54:59 +0100

    Initial commit

---
+++ src/DN01.java
@@ -0,0 +1,14 @@
+public class DN01 {
+    public static void main(String[] args) {
+        System.out.println("Test ŽČŠ");
+        System.out.println("""
+                Moje ime je: Žiga Slatnar Štagar
+                Moja vpisna številka: 63230303
+                                
+                ***********************************************
+                *|*  Izjavljam,  da bom vse obveznosti pri  *|*
+                *|*  Programiranju 2 opravil/a samostojno.  *|*
+                ***********************************************
+                """);
+    }
+}
