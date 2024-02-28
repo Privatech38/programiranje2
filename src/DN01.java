19a6566d601fcd0459d7458a08be69b05a26b5d7
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-02-28 18:23:44 +0100

    Removed unnecessary output

--- src/DN01.java
+++ src/DN01.java
@@ -1,6 +1,5 @@
 public class DN01 {
     public static void main(String[] args) {
-        System.out.println("Test ŽČŠ");
         System.out.println("""
                 Moje ime je: Žiga Slatnar Štagar
                 Moja vpisna številka: 63230303
