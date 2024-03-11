ea2528f945a8907add2cb9c88135906ebbb8472e
Privatech <ziga.slatnar.stagar@gmail.com>
2024-03-11 12:55:17 +0100

    Dodatne naloge trikotniki

--- src/vaje/vaje1/Trikotniki.java
+++ src/vaje/vaje1/Trikotniki.java
@@ -14,6 +14,8 @@ public class Trikotniki {
         romb(2, 5);
         smreka(3);
         smreka(4);
+        rombA(1, 5);
+        rombPrazen(3, 5);
     }
 
     public static void trikotnikStevil(int visina) {
@@ -94,6 +96,81 @@ public class Trikotniki {
             trikotnik((velikost - i) * 2, 2 * i);
         }
         Pravokotniki.pravokotnik(velikost + 1, (velikost % 2 == 0 ? velikost + 1 : velikost), 2 * velikost);
+    }
+
+    public static void rombA(int odmik, int velikost) {
+        for (int i = 1; i <= velikost; i++) {
+            System.out.println();
+            for (int j = 0; j < odmik; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (velikost - i) * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("# ");
+            }
+        }
+        for (int i = velikost - 1; i > 0; i--) {
+            System.out.println();
+            for (int j = 0; j < odmik + 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (velikost - i - 1) * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("# ");
+            }
+        }
+    }
+
+    public static void rombPrazen(int odmik, int velikost) {
+        System.out.println();
+        for (int i = 0; i < odmik; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < velikost * 2 - 1; i++) {
+            System.out.print("# ");
+        }
+        System.out.println();
+        for (int i = 0; i < velikost - 1; i++) {
+            for (int j = 0; j < odmik; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < velikost - 1 - i; j++) {
+                System.out.print("# ");
+            }
+            for (int j = 0; j < 1 + 2 * i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < velikost - 1 - i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+        for (int i = velikost - 3; i >= 0; i--) {
+            for (int j = 0; j < odmik; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < velikost - 1 - i; j++) {
+                System.out.print("# ");
+            }
+            for (int j = 0; j < 1 + 2 * i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < velikost - 1 - i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < odmik; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < velikost * 2 - 1; i++) {
+            System.out.print("# ");
+        }
+
     }
 
 }
