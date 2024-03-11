53890cfdb5f6beeb8acf91201e63a577cf1c56af
Privatech <ziga.slatnar.stagar@gmail.com>
2024-03-11 11:39:38 +0100

    Pravokotniki done

---
+++ src/vaje/Pravokotniki.java
@@ -0,0 +1,39 @@
+package vaje;
+
+public class Pravokotniki {
+
+    public static void main(String[] args) {
+        pravokotnikStevil(7, 3);
+        pravokotnikStevil(12, 3);
+        pravokotnik(5,7,3);
+    }
+
+    public static void pravokotnikStevil(int sirina, int visina) {
+        for (int i = 1; i <= visina; i++) {
+            System.out.print("\n");
+            for (int j = 1; j <= sirina; j++) {
+                int current = i;
+                if (sirina >= 10) {
+                    current = j;
+                    if (j >= 10) {
+                        current -= 10;
+                    }
+                }
+                System.out.print(current);
+            }
+        }
+    }
+
+    public static void pravokotnik(int odmik, int sirina, int visina) {
+        for (int i = 0; i < visina; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < odmik; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < sirina; j++) {
+                System.out.print("X");
+            }
+        }
+    }
+
+}
